fa15b3ace54b62f4aecf99b07c9f5d9946506eab
Thomas Maul <Thomas.Maul@4d.com>
2024-06-18 14:13:59 +0200

    Add German translation

--- src/components/Calendar/Calendar.settings.ts
+++ src/components/Calendar/Calendar.settings.ts
@@ -23,6 +23,7 @@ const commonSettings: TSetting[] = [
       { value: 'en', label: 'English' },
       { value: 'fr', label: 'French' },
       { value: 'es', label: 'Spanish' },
+      { value: 'de', label: 'German' },
     ],
   },
   {
